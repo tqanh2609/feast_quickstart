e7de0e8e0a65f13976d5c426bca2dfd8cca4b408
root <root@tranquanganh.localdomain>
2025-05-05 09:41:46 +0700

    comment feast teardown

--- test_workflow.py
+++ test_workflow.py
@@ -24,37 +24,37 @@ def run_demo():
     print("\n--- Online features ---")
     fetch_online_features(store)
 
-    print("\n--- Online features retrieved (instead) through a feature service---")
-    fetch_online_features(store, source="feature_service")
+    # print("\n--- Online features retrieved (instead) through a feature service---")
+    # fetch_online_features(store, source="feature_service")
 
-    print(
-        "\n--- Online features retrieved (using feature service v3, which uses a feature view with a push source---"
-    )
-    fetch_online_features(store, source="push")
+    # print(
+    #     "\n--- Online features retrieved (using feature service v3, which uses a feature view with a push source---"
+    # )
+    # fetch_online_features(store, source="push")
 
-    print("\n--- Simulate a stream event ingestion of the hourly stats df ---")
-    event_df = pd.DataFrame.from_dict(
-        {
-            "driver_id": [1001],
-            "event_timestamp": [
-                datetime.now(),
-            ],
-            "created": [
-                datetime.now(),
-            ],
-            "conv_rate": [1.0],
-            "acc_rate": [1.0],
-            "avg_daily_trips": [1000],
-        }
-    )
-    print(event_df)
-    store.push("driver_stats_push_source", event_df, to=PushMode.ONLINE_AND_OFFLINE)
+    # print("\n--- Simulate a stream event ingestion of the hourly stats df ---")
+    # event_df = pd.DataFrame.from_dict(
+    #     {
+    #         "driver_id": [1001],
+    #         "event_timestamp": [
+    #             datetime.now(),
+    #         ],
+    #         "created": [
+    #             datetime.now(),
+    #         ],
+    #         "conv_rate": [1.0],
+    #         "acc_rate": [1.0],
+    #         "avg_daily_trips": [1000],
+    #     }
+    # )
+    # print(event_df)
+    # store.push("driver_stats_push_source", event_df, to=PushMode.ONLINE_AND_OFFLINE)
 
-    print("\n--- Online features again with updated values from a stream push---")
-    fetch_online_features(store, source="push")
+    # print("\n--- Online features again with updated values from a stream push---")
+    # fetch_online_features(store, source="push")
 
-    print("\n--- Run feast teardown ---")
-    subprocess.run(["feast", "teardown"])
+    # print("\n--- Run feast teardown ---")
+    # subprocess.run(["feast", "teardown"])
 
 
 def fetch_historical_features_entity_df(store: FeatureStore, for_batch_scoring: bool):
